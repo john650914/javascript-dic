7b52e44852a68bab580004867745bc2f4205def6
ftc-John-Chang <john.chang@fullerton.com.tw>
2017-01-06 10:35:21 +0800

    把GA實驗碼拿掉 改首頁說明wording sitemap加入Set.php、Map.php 改一點CSS

--- index.php
+++ index.php
@@ -2,20 +2,6 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<!-- Google Analytics Content Experiment code -->
-<script>function utmx_section(){}function utmx(){}(function(){var
-k='114137935-0',d=document,l=d.location,c=d.cookie;
-if(l.search.indexOf('utm_expid='+k)>0)return;
-function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
-indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
-length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
-'<sc'+'ript src="'+'http'+(l.protocol=='https:'?'s://ssl':
-'://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
-'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
-valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
-'" type="text/javascript" charset="utf-8"><\/sc'+'ript>')})();
-</script><script>utmx('url','A/B');</script>
-<!-- End of Google Analytics Content Experiment code -->
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta property="og:title" content="Javascript字典" />
@@ -42,15 +28,9 @@ valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
 ?>
 <main class="main">
 	<h1 class="index-h1" style="text-indent:-13px;">「Javascript字典」是一本好書嗎？</h1>
-	<p>(還有什麼好寫的？)基於網頁瀏覽器的撰寫方向，不包涵node.js的非瀏覽器應用 | 來源：網路上的資料、書本、自己實驗的結果 | 目的：自己學習的記錄 | 期望它變成：大家學習、找資料的地方 | 現況：需要改善專業度、修正錯誤及補齊資料</p>
-	<p style="height:0px; overflow:hidden;">Javascript是一種物件導向的語言，語法裡的一切幾乎都是以物件的方式操作，有人預言Javascript會是<em>未來唯一的語言</em>。</p>
-	<style>
-		.test_search{position:relative;}
-		.test_search img{position:absolute; left:0; top:0; width:1px; height:1300px; border-right:1px solid red;}
-	</style>
-	<p class="test_search">它是以ECMA做標準，目前已有更新的版本，有一種永遠都學不完的感覺，誰叫他有可能是一統天下的程式語言。(這一段和上一段是在2016/020/24加上的)<img src="images/test.gif" /></p>
-	<div id="data-loader"></div>
-	<div id="data-loader2"></div>
+	<p>Javascript是一種物件導向的語言，多應用於網頁開發，語法裡的一切幾乎都是以物件的方式操作，有人預言Javascript會是<em>未來唯一的語言</em>，它是以ECMA做標準，目前ES6是最新的版本，有一種永遠都學不完的感覺，，誰叫他有可能是一統天下的程式語言。</p>
+	<p>字典裡的所有資訊皆來自於網路上的教學、書本、自己實驗的結果，目的是記錄自己的學習歷程，未來希望它變成大家學習、找資料的地方；目前在專業度及資料量還有許多需要補足的地方，有問題也請多包涵。</p>
+	<p>本書所記載的資料是針對瀏覽器的API，不包涵node.js等的非瀏覽器應用。</p>
 </main>
 </body>
 </html>
