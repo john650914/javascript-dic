ffda44c86c7f7f6fc0e6b7210ebc1f177160c19e
John Chang <john650914@gmail.com>
2017-01-08 12:19:17 +0800

    新增adsense code

---
+++ includes/adsense.php
@@ -0,0 +1,12 @@
+<!-- Adsense -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Javascript dic AD -->
+<ins class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-1473254466018182"
+    data-ad-slot="5590684686"
+    data-ad-format="link"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<!-- Adsense -->
